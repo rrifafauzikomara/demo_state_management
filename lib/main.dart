1766fabc5af333cb10dc67ba68ac4f1a1ec387d3
rrifafauzikomara <rifafauzi6@gmail.com>
2022-02-20 12:29:54 +0700

    add default screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:demo_state_management/example_bloc/ui/login_with_bloc_screen.dart';
-import 'package:demo_state_management/example_without_bloc/login_without_bloc_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,25 +7,63 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const LoginWithoutBlocScreen(),
+      home: const DefaultScreen(title: 'Flutter Demo'),
+      // home: const LoginWithoutBlocScreen(),
       // home: const LoginWithBlocScreen(),
+    );
+  }
+}
+
+class DefaultScreen extends StatefulWidget {
+  const DefaultScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<DefaultScreen> createState() => _DefaultScreenState();
+}
+
+class _DefaultScreenState extends State<DefaultScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
